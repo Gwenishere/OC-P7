f13db004574ee02fa54aad8f587763afbbdbea2f
Gwen <gwenteste@yahoo.fr>
2020-11-15 00:53:01 +0100

    front
    tests React

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+// import Header from './components/Header/Header';
+// import Signup from './components/Signup';
+// import Login from './components/Login';
+// import Home from './Home/Home/Home';
+// brancher à React DOM ?
+
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
+// UTILISER AXIOS POUR LES REQUETES 
+
+
+function App() {
+  return (
+    <div>
+      <h1>Coucou</h1>
+    </div>
+  )}
+export default App;
+
+
+
