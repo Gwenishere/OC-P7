3351e3210bb52c67a589faea44030004bac04448
Gwen <gwenteste@yahoo.fr>
2020-11-18 23:22:48 +0100

    fichier front
    ajustement fichiers lies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; //ok
+import ReactDOM from 'react-dom'; // ok
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// pour rendre qqun chose sur l'écran ReactDom.render
+// on mets ce que je veux envoyer sur l'écran et où je vais le faire
+ReactDOM.render(<App/>, document.getElementById('root'));
