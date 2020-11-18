46f60a2172d3b52b823c59a5f4acd4bb8569f241
Gwen <gwenteste@yahoo.fr>
2020-11-18 23:08:06 +0100

    fichier front
    footer mise en ligne class

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Footer extends React.Component
+{
+    render ()
+    {
+        return (
+        <footer class="jumbotron">
+            <ul>
+                <li>Support technique</li>
+                <li>Conditions d'utilisation</li>
+                <li>Groupomania le site officiel</li>
+            </ul>
+        </footer>
+        )
+    }
+}
+  export default Footer
