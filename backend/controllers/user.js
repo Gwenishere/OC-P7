00220d6717ae5252ac7cf9126517f89b32df1355
Gwen <gwenteste@yahoo.fr>
2020-12-13 00:28:28 +0100

    Back controllers user
    ajout requete sql, ok recup dans table A voir login et Admin ici ou a part ?

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,11 +2,14 @@ const bcrypt = require('bcrypt'); // installé
 const jwt = require('jsonwebtoken'); // installé
 const mysql = require('mysql'); // installé
 const User = require('../models/entity/user.js'); // import modele user 
-
+const db = require('../config/db');
 
 exports.signup = (req, res) => {
+  console.log("message de signup");
   let user = new User(req.body.user);
-
+  const username = req.body.user.username;
+  const email = req.body.user.email;
+  const password = req.body.user.password;
   if(!user.checkpassword()) {
         return res.status(400).json({
             message: 'le mot de passe doit contenir 1 minuscule, 1 majuscule, 1 chiffre, 8 caractères'
@@ -17,41 +20,41 @@ exports.signup = (req, res) => {
           message: "Le champs doit être complété"
       });
   };
-    // hash le mot de passe
-    bcrypt.hash(user.password, 10)
+  bcrypt.hash(user.password, 10) // hash le mot de passe
      .then(hash => {
      user.password = hash;
-
     // envoyer les ref utilisateur vers la base de données
-    //**FIXME: pas compris la séquence avec manager ! que dois-je faire? */
-    User.manager.create(user, (err) => {
-    if (err) {
-      res.status(500).send({
-        message: "problème lors de la création du compte"
-      });
+ //**FIXME: pas compris la séquence avec manager ! que dois-je faire? */ /* User.manager.create(user, (err) => { if (err) { res.status(500).send({  message: "problème lors de la création du compte" });}})*/
+   })
+    if (!req.body.user.username && !req.body.user.password && !req.body.user.email) {
+      res.status(401).json({ message: 'Valeur manquante !'});
     }
-   })
-   })
-}
+    db.query(
+        "INSERT INTO user (username, email, password) VALUES (?, ?, ?);", [username, email, password], 
+        (err, results)=> {
+          console.log(err);
+          res.send(results);
+        }
+    );
+  }
 /**TODO:
 * bloquer au bout de 5 tentatives du mdp ?
 */
-
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    User.findOne({ email: req.body.user.email })
       .then(user => {
         if (!user) {
           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
-        bcrypt.compare(req.body.password, user.password)
+        bcrypt.compare(req.body.user.password, user.password)
           .then(valid => {
             if (!valid) {
               return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
             res.status(200).json({
-              userId: user._id,
+              userId: user.user_id,
               token: jwt.sign(
-                  { userId: user._id },
+                  { userId: user.user_id },
                   'RANDOM_TOKEN_SECRET', // simple chaine
                   { expiresIn: '24h' }
               )
