67f63196c499568246c6a2c126d96231a16379e3
Gwen <gwenteste@yahoo.fr>
2020-12-12 13:45:27 +0100

    multer config objet storage modif
    modif export module

--- backend/middelware/multer-config.js
+++ backend/middelware/multer-config.js
@@ -6,10 +6,11 @@ const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/gif': 'gif'
 };
-
+// config de l'objet qui sera stocké
 const storage = multer.diskStorage({
+// destination d'enregistrement des fichiers postés par user
     destination: (req, file, callback) => {
-        callback(null, 'images')
+        callback(null, 'images');
     },
     filename: (req, file, callback) =>{
         const name = file.originalname.replace(/\..+$/, '').split(' ').join('_'); // nom fichier
@@ -17,5 +18,5 @@ const storage = multer.diskStorage({
         callback(null, name + Date.now() + '.' + extension);
     }
 });
-
-module.exports = multer({ storage }).single('image');
+// export du module objet storage, méthode single
+module.exports = multer({ storage: storage }).single('image');
